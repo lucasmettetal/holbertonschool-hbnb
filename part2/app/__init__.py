f681e99e6aab52151a439aedd1e826850e4c1ed1
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-02-24 13:00:22 +0000

    feat: register places and reviews namespaces in HBnB API

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,6 +3,8 @@ from flask_restx import Api
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
 
 
 def create_app():
@@ -19,5 +21,7 @@ def create_app():
     # Register namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(amenities_ns, path='/api/v1/places')
+    api.add_namespace(amenities_ns, path='/api/v1/reviews')
 
     return app
