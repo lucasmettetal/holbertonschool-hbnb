94a6e7e7ecdba6d210224340cf9c2a8385d8a307
Lucas Mettetal <lucas.mettetal2@gmail.com>
2026-02-24 13:03:12 +0000

    fix: correct namespace registration for places and reviews in HBnB API

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -21,7 +21,7 @@ def create_app():
     # Register namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(amenities_ns, path='/api/v1/places')
-    api.add_namespace(amenities_ns, path='/api/v1/reviews')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
